d5e6ec16536e9590eaf77fb0393bbf0f9e3aa623
AndreyZelenskiy <zvukach70@gmail.com>
2016-10-13 02:54:53 +0300

    Add recounting test

--- main/GameField/gameField_test.go
+++ main/GameField/gameField_test.go
@@ -14,6 +14,7 @@ var tests = []testPair{
 	{getIsInFieldResult(11, 11), false},
 	{getIsInFieldResult(1, 15), false},
 	{getIsInFieldResult(1123, -1), false},
+	{getRecalculationResult(), true},
 }
 
 func TestGameField(t *testing.T){
@@ -32,5 +33,20 @@ func getIsInFieldResult(x, y int) bool{
 	gameField := New()
 	return gameField.isInField(x, y)
 }
-//func getRecalculationResult(){}
+func getRecalculationResult() bool{
+	gameField := New()
+	field := [8][8]int{
+		{0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 1, 2, 2, 2, 1, 0},
+		{0, 0, 0, 2, 1, 2, 1, 0},
+		{0, 0, 0, 1, 0, 0, 0, 0},
+		{0, 0, 1, 2, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	gameField.Field = field
+	gameField.recalculation()
+	return gameField.player2_scores == 6 && gameField.player1_scores == 7
+}
 //func getMakeStepResult(){}
